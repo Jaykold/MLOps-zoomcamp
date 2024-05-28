874b77b2e948e110abfeef10a9341cec535ee99e
Christian <christianichebi@gmail.com>
2024-05-28 19:25:37 +0100

    Succeed

---
+++ 02-experiment-tracking/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+try:
+                
+            #     new_params[param] = int(params[param])
+            # except ValueError:
+            #     print(f"Invalid value for {param}: {params[param]}")
+            #     new_params[param] = params[param]
