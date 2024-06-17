551c378df62c0acc9741bbf79b62991fe3aa54e5
Christian <christianichebi@gmail.com>
2024-06-18 00:48:07 +0100

    homework-04

---
+++ 04-deployment/homework/starter.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+import sys
+
+# In[3]:
+
+
+#pip freeze | findstr scikit-learn
+
+
+# In[4]:
+
+
+print(sys.version)
+
+
+# In[5]:
+
+
+import pickle
+import pandas as pd
+
+
+# In[6]:
+
+
+with open('model.bin', 'rb') as f_in:
+    dv, model = pickle.load(f_in)
+
+
+# In[7]:
+
+
+categorical = ['PULocationID', 'DOLocationID']
+
+def read_data(filename):
+    df = pd.read_parquet(filename)
+    
+    df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
+    df['duration'] = df.duration.dt.total_seconds() / 60
+
+    df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
+
+    df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
+    
+    return df
+
+
+# In[9]:
+
+
+df = read_data('https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-04.parquet')
+
+
+# In[10]:
+
+
+dicts = df[categorical].to_dict(orient='records')
+X_val = dv.transform(dicts)
+y_pred = model.predict(X_val)
+
+
+# In[11]:
+
+
+df['y_pred'] = y_pred
+print(df['y_pred'].describe()['mean'])
+
+
+# In[ ]:
+
+
+
+
